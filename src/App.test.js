df4cd91c11f4557054546b078c58582c5c595785
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-09-12 15:18:21 +1000

    Adding describe block to app test.

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
 
-it('does not explode', () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('App', () => {
+  it('does not explode', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
